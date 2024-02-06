76efb0c88b43145227a4e312e59df4536d5c7db7
Zyber Loshka <75445321+Zyb3rWolfi@users.noreply.github.com>
2024-02-06 10:12:02 +0000

    Setting up players

---
+++ My project/Assets/Scripts/playerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
